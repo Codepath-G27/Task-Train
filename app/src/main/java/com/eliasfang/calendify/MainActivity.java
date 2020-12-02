ce4b76a56d6e1deb8e985875b30d7b6dc35b00bf
Elias Fang <56064410+eliasfang@users.noreply.github.com>
2020-12-01 21:55:00 -0800

    Fix code so that app launches successfully (#27)
    * added kotlin to gradle
    
    * Create MainActivity + layout and remodel CalendarFragment + assets
    
    * Fixed typo in drawable and changed Button to FloatingActionButton

---
+++ app/src/main/java/com/eliasfang/calendify/MainActivity.java
@@ -0,0 +1,46 @@
+package com.eliasfang.calendify;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    final FragmentManager fragmentManager = getSupportFragmentManager();
+    private BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment;
+                switch (item.getItemId()) {
+                    case R.id.action_todo:
+                        fragment = new ToDoFragment();
+                        break;
+                    case R.id.action_calendar:
+                    default:
+                        fragment = new CalendarFragment();
+                        break;
+                }
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                return true;
+            }
+        });
+        // default view
+        bottomNavigationView.setSelectedItemId((R.id.action_calendar));
+    }
+}
