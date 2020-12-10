c80344ff87b82d02613378f63e02630aff1ac0c1
Elias Fang <56064410+eliasfang@users.noreply.github.com>
2020-12-10 11:33:49 -0800

    Update creation dialogs (#49)

--- app/src/main/java/com/eliasfang/calendify/TaskCreateDialogFragment.java
+++ app/src/main/java/com/eliasfang/calendify/TaskCreateDialogFragment.java
@@ -6,17 +6,24 @@ import androidx.fragment.app.DialogFragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
+
+import java.util.Calendar;
 
 import static android.app.Activity.RESULT_OK;
 
@@ -24,11 +31,10 @@ public class TaskCreateDialogFragment extends DialogFragment implements View.OnC
     public static final String EXTRA_REPLY =
             "com.eliasfang.calendify.TASK";
 
-    public static final int TASK_CREATION_FRAGMENT = 1;
     private EditText etTitle;
     private EditText etLocation;
-    private TextView tvDate;
-    private TextView tvTime;
+    private Button btnDate;
+    private Button btnTime;
     private CheckBox cbRecur;
     private EditText etDescription;
 
@@ -56,8 +62,8 @@ public class TaskCreateDialogFragment extends DialogFragment implements View.OnC
         TextView tvSave = view.findViewById(R.id.tvSave);
         etTitle = view.findViewById(R.id.etTitle);
         etLocation = view.findViewById(R.id.etLocation);
-        tvDate = view.findViewById(R.id.tvDate);
-        tvTime = view.findViewById(R.id.tvTime);
+        btnDate = view.findViewById(R.id.btnDate);
+        btnTime = view.findViewById(R.id.btnTime);
         cbRecur = view.findViewById(R.id.cbRecur);
         etDescription = view.findViewById(R.id.etDescription);
 
@@ -65,9 +71,11 @@ public class TaskCreateDialogFragment extends DialogFragment implements View.OnC
         etTitle.requestFocus();
         getDialog().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
 
-        // Check for clicks on top buttons
+        // Check for clicks on buttons
         imgBtnClose.setOnClickListener(this);
         tvSave.setOnClickListener(this);
+        btnDate.setOnClickListener(this);
+        btnTime.setOnClickListener(this);
 
         return view;
     }
@@ -78,12 +86,18 @@ public class TaskCreateDialogFragment extends DialogFragment implements View.OnC
 
         // Manage which button was clicked on event creation dialog
         switch (id) {
+            case R.id.btnDate:
+                saveDate();
+                break;
+            case R.id.btnTime:
+                saveTime();
+                break;
             case R.id.imgBtnClose:
                 dismiss();
                 Toast.makeText(getContext(), "Cancelled task creation", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.tvSave:
-                Task task = saveFields();
+                Task task = saveData();
                 Intent replyIntent = new Intent();
                 replyIntent.putExtra(EXTRA_REPLY, task);
                 TaskViewModel myTaskViewModel = new ViewModelProvider(this).get(TaskViewModel.class);
@@ -95,13 +109,63 @@ public class TaskCreateDialogFragment extends DialogFragment implements View.OnC
         }
     }
 
+    private void saveDate() {
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+        DatePickerDialog datePickerDialog = new DatePickerDialog(getContext(), new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                btnDate.setText(day + "-" + (month + 1) + "-" + year);
+            }
+        }, year, month, day);
+        datePickerDialog.show();
+    }
+
+    private void saveTime() {
+        Calendar calendar = Calendar.getInstance();
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        int minute = calendar.get(Calendar.MINUTE);
+        TimePickerDialog timePickerDialog = new TimePickerDialog(getContext(), new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker timePicker, int i, int i1) {
+                btnTime.setText(FormatTime(i, i1));
+            }
+        }, hour, minute, false);
+        timePickerDialog.show();
+    }
+
+    public String FormatTime(int hour, int minute) {
+
+        String time;
+        String formattedMinute;
+
+        if (minute / 10 == 0) {
+            formattedMinute = "0" + minute;
+        } else {
+            formattedMinute = "" + minute;
+        }
+        if (hour == 0) {
+            time = "12" + ":" + formattedMinute + " AM";
+        } else if (hour < 12) {
+            time = hour + ":" + formattedMinute + " AM";
+        } else if (hour == 12) {
+            time = "12" + ":" + formattedMinute + " PM";
+        } else {
+            int temp = hour - 12;
+            time = temp + ":" + formattedMinute + " PM";
+        }
+        return time;
+    }
+
     // Helper functions to save the fields that are filled in
-    private Task saveFields() {
+    private Task saveData() {
         //todo add some error handling to make sure that we have a valid task
         String title = etTitle.getText().toString();
         String location = etLocation.getText().toString();
-        String date = tvDate.getText().toString();
-        String time = tvTime.getText().toString();
+        String date = btnDate.getText().toString();
+        String time = btnTime.getText().toString();
         boolean recur = cbRecur.isChecked();
         String description = etDescription.getText().toString();
 
